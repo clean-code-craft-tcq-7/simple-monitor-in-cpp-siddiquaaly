4317f1968d5a25111a7205a37639c1ca74f565b7
siddiquaaly <alysid786@gmail.com>
2025-09-28 15:27:12 +0000

    removed using namespace reference from header file

--- monitor.h
+++ monitor.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <vector>
 #include <string>
-using std::cout, std::vector,std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 struct vitalRange{
   std::string name;
@@ -12,6 +11,6 @@ struct vitalRange{
 extern const std::vector<vitalRange> vitals;
 
 void  vitalsCriticalAttention(void);
-bool isVitalsNormal(const vector<float>& currentValues);
+bool isVitalsNormal(const std::vector<float>& currentValues);
 bool isSingleVitalNormal(float currentValue, const vitalRange& vital);
 
