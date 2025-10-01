4125e68d46d0f5b876b5e2209746175827860831
siddiquaaly <alysid786@gmail.com>
2025-10-01 10:09:00 +0000

    added semicolon in the testcases

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -11,42 +11,42 @@ TEST(MonitorTemperature, TemperatureIsHigh) {
 
 TEST(Monitor_Temperature, TemperatureIsLow) { 
   // Current vital values: Temperature, PulseRate, SpO2
-  vector<float> currentValues = {90.5,80,95}
+  vector<float> currentValues = {90.5,80,95};
   EXPECT_FALSE(isVitalsNormal(currentValues)) << "Temp is low, condition is critical";
 }
 
 TEST(Monitor_Temperature, TemperatureIsNormal) { 
   // Current vital values: Temperature, PulseRate, SpO2
-  vector<float> currentValues = {98.8,80,95}
+  vector<float> currentValues = {98.8,80,95};
   EXPECT_TRUE(isVitalsNormal(currentValues)) << "Temp is Normal, All OK";
 }
 
 TEST(Monitor_PulseRate, PulseRateIsHigh) { 
   // Current vital values: Temperature, PulseRate, SpO2
-  vector<float> currentValues = {98,102,95}
+  vector<float> currentValues = {98,102,95};
   EXPECT_FALSE(isVitalsNormal(currentValues)) << "PulseRate is high, condition is critical";
 }
 
 TEST(Monitor_PulseRate, PulseRateIsLow) { 
   // Current vital values: Temperature, PulseRate, SpO2
-  vector<float> currentValues = {96,59,95}
+  vector<float> currentValues = {96,59,95};
   EXPECT_FALSE(isVitalsNormal(currentValues)) << "PulseRate is low, condition is critical";
 }
 
 TEST(Monitor_PulseRate, PulseRateIsNormal) { 
   // Current vital values: Temperature, PulseRate, SpO2
-  vector<float> currentValues = {100,80,95}
+  vector<float> currentValues = {100,80,95};
   EXPECT_TRUE(isVitalsNormal(currentValues)) << "PulseRate is Normal, All OK";
 }
 
 TEST(Monitor_Sp02, Sp02IsLow) { 
   // Current vital values: Temperature, PulseRate, SpO2
-  vector<float> currentValues = {96,80,88}
+  vector<float> currentValues = {96,80,88};
   EXPECT_FALSE(isVitalsNormal(currentValues)) << "Sp02 is low, condition is critical";
 }
 
 TEST(Monitor_Sp02, Sp02IsNormal) { 
   // Current vital values: Temperature, PulseRate, SpO2
-  vector<float> currentValues = {100,80,95}
+  vector<float> currentValues = {100,80,95};
   EXPECT_TRUE(isVitalsNormal(currentValues)) << "Sp02 is Normal, All OK";
 }
